7bc75ec761863b5bf9d91401b2e7295232329924
Joseph Rafael Ferrer <rafael2x0@gmail.com>
2026-04-23 10:28:54 +0800

    Add benchmark for the iterators

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -1,10 +1,25 @@
 use criterion::{Criterion, criterion_group, criterion_main};
 use std::hint::black_box;
-use tsuru_challenge::Timestamp;
+use tsuru_challenge::{PcapIterator, Timestamp, build_quote_iterator, open_mmaped_file};
 
 fn criterion_benchmark(c: &mut Criterion) {
     c.bench_function("parse_hhmmssuu", |b| {
         b.iter(|| Timestamp::parse_hhmmssuu(black_box(b"12304580")))
+    });
+    c.bench_function("PCAP iterator", |b| {
+        b.iter(|| {
+            let mmap = open_mmaped_file(black_box("mdf-kospi200.20110216-0.pcap")).unwrap();
+            let iterator = PcapIterator::new(&mmap);
+            iterator.count()
+        })
+    });
+    c.bench_function("quote iterator", |b| {
+        b.iter(|| {
+            let mmap = open_mmaped_file(black_box("mdf-kospi200.20110216-0.pcap")).unwrap();
+            let pcap_iterator = PcapIterator::new(&mmap);
+            let quote_iterator = build_quote_iterator(pcap_iterator);
+            quote_iterator.count()
+        })
     });
 }
 
