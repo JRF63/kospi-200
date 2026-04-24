160edde973e9d3472c78da2d1aa867c56438d22c
Joseph Rafael Ferrer <rafael2x0@gmail.com>
2026-04-24 19:47:53 +0800

    Switch to the bucket sorting implementation

--- printer/src/main.rs
+++ printer/src/main.rs
@@ -1,10 +1,8 @@
 use clap::Parser;
 use std::io::{BufWriter, Write};
 
-use kospi_parser::{PcapIterator, QuoteIterator, SortedQuoteIteratorHeap, open_mmaped_file};
+use kospi_parser::{PcapIterator, QuoteIterator, SortedQuoteIteratorBuckets, open_mmaped_file};
 
-const APPROX_PACKETS_PER_SEC: usize = 1000; // Assume 1000 packets per second
-const INITIAL_HEAP_CAPACITY: usize = 3 * APPROX_PACKETS_PER_SEC; // 3 second buffer
 const STDOUT_BUF_SIZE: usize = 128_000; // Use a large value to minimize syscalls
 
 #[derive(Parser)]
@@ -26,7 +24,7 @@ fn main() -> std::io::Result<()> {
     let mut writer = BufWriter::with_capacity(STDOUT_BUF_SIZE, std::io::stdout().lock());
 
     if args.reorder {
-        let quote_iterator = SortedQuoteIteratorHeap::new(pcap_iterator, INITIAL_HEAP_CAPACITY);
+        let quote_iterator = SortedQuoteIteratorBuckets::new(pcap_iterator);
 
         // Prints the quote in order of ascending accept time
         for quote in quote_iterator {
